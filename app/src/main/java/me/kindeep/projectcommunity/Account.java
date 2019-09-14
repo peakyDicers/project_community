b709bfe033e00210fd8c2802d409eea31aa6e11e
peakyDicers <ian.chui@hotmail.com>
2019-09-14 00:09:52 -0400

    created account obj.

---
+++ app/src/main/java/me/kindeep/projectcommunity/Account.java
@@ -0,0 +1,18 @@
+package me.kindeep.projectcommunity;
+
+/**
+ * Author: admin
+ * Date: 2019-09-14
+ */
+public class Account {
+
+    String firstName;
+    String lastName;
+    String id;
+
+    public Account(String firstName, String lastName, String id){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+}
